0ad8092d5bf3aa7f4522fd4b79041c253663b690
Rodrigo Croci Costa <rodrigo.croci@gmail.com>
2021-05-03 10:41:40 -0300

    Codando

---
+++ AppMvcCompleta/src/DevIO.Data/Repository/Repository.cs
@@ -0,0 +1,63 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+    {
+        protected readonly MeuDbContext Db;
+        protected readonly DbSet<TEntity> DbSet;
+
+        public async Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public async Task<TEntity> ObterPorID(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<List<TEntity>> ObterTodos()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public async Task Adicionar(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await SaveChanges();
+        }
+
+        public async Task Atualizar(TEntity entity)
+        {
+            DbSet.Update(entity);
+            await SaveChanges();
+        }
+
+        public async Task Remover(Guid id)
+        {
+            DbSet.Remove(new TEntity { Id = id });
+            await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Db?.Dispose();
+        }
+
+    }
+}
