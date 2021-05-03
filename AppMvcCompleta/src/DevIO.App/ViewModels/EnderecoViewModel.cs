26abd2d342efa2a0108d4af188e7671f03c13886
Rodrigo Croci Costa <rodrigo.croci@gmail.com>
2021-05-03 12:11:21 -0300

    Codando

---
+++ AppMvcCompleta/src/DevIO.App/ViewModels/EnderecoViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.App.ViewModels
+{
+    public class EnderecoViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório !!!")]
+        [StringLength(200, ErrorMessage = "O Campo {0} deve ter entre {2} e {1} caracteres...", MinimumLength = 2)]
+        public string Logradouro { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório !!!")]
+        [StringLength(50, ErrorMessage = "O Campo {0} deve ter entre {2} e {1} caracteres...", MinimumLength = 1)]
+        public string Numero { get; set; }
+
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório !!!")]
+        [StringLength(8, ErrorMessage = "O Campo {0} deve ter entre {1} caracteres...", MinimumLength = 8)]
+        public string Cep { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório !!!")]
+        [StringLength(100, ErrorMessage = "O Campo {0} deve ter entre {2} e {1} caracteres...", MinimumLength = 2)]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório !!!")]
+        [StringLength(200, ErrorMessage = "O Campo {0} deve ter entre {2} e {1} caracteres...", MinimumLength = 2)]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório !!!")]
+        [StringLength(50, ErrorMessage = "O Campo {0} deve ter entre {2} e {1} caracteres...", MinimumLength = 2)]
+        public string Estado { get; set; }
+
+        //EF Relation
+        [HiddenInput]
+        public FornecedorViewModel fornecedor { get; set; }
+    }
+}
---
+++ AppMvcCompleta/src/DevIO.App/ViewModels/FornecedorViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.App.ViewModels
+{
+    public class FornecedorViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório !!!")]
+        [StringLength(100, ErrorMessage = "O Campo {0} deve ter entre {2} e {1} caracteres...", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório !!!")]
+        [StringLength(14, ErrorMessage = "O Campo {0} deve ter entre {2} e {1} caracteres...", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        [DisplayName("Tipo")]
+        public int TipoFornecedor { get; set; }
+
+        [DisplayName("Endereço")]
+        public EnderecoViewModel Endereco { get; set; }
+
+        [DisplayName("Ativo ?")]
+        public bool Ativo { get; set; }
+
+        //EF Relations
+        public IEnumerable<ProdutoViewModel> Produtos { get; set; }
+    }
+}
